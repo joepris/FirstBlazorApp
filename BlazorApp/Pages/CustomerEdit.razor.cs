cb64b6bf60040057603cfd3defe02a1802155e3d
JoePris <calpito.jp@gmail.com>
2024-01-05 22:24:25 -0700

    before DB

--- BlazorApp/Pages/CustomerEdit.razor.cs
+++ BlazorApp/Pages/CustomerEdit.razor.cs
@@ -9,20 +9,5 @@ namespace FirstBlazorApp.Pages
 
         public string? CustomerId { get; set; }
         public Customer Customer { get; set; } = new Customer();
-        private void SaveCustomer(Customer savedCustomer)
-        {
-            Customer newCustomer = new Customer()
-            {
-                CustomerId = 3,
-                FirstName = savedCustomer.FirstName,
-                LastName = savedCustomer.LastName,
-                Age = savedCustomer.Age,
-                Gender = savedCustomer.Gender,
-                MaritalStatus = savedCustomer.MaritalStatus,
-                Email = savedCustomer.Email,
-                PhoneNumber = savedCustomer.PhoneNumber,
-            };
-            return 
-        }
     }
 }
